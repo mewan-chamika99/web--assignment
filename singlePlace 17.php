67ee86d8c9bd0b12bcad258662d12b8becc5e56d
Chamath Hasaranga <97539630+Chamath1999@users.noreply.github.com>
2022-02-25 01:29:43 +0530

    Update singlePlace 17.php

--- singlePlace 17.php
+++ singlePlace 17.php
@@ -21,7 +21,7 @@
 
             ?>
 
-            <div class="col-12 singleHd " style="margin-bottom: 40px;">
+            <div class="col-12 a17 " style="margin-bottom: 40px;">
                 <h1 class="text-center pt-5">St Lucia Caribbean Island</h1>
             </div>
 
