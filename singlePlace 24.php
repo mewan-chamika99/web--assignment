72b89f60ec77781e86b2be1911c240187f43841f
Chamath Hasaranga <97539630+Chamath1999@users.noreply.github.com>
2022-02-25 01:34:14 +0530

    Update singlePlace 24.php

--- singlePlace 24.php
+++ singlePlace 24.php
@@ -21,7 +21,7 @@
 
             ?>
 
-            <div class="col-12 singleHd " style="margin-bottom: 40px;">
+            <div class="col-12 a24 " style="margin-bottom: 40px;">
                 <h1 class="text-center pt-5">Vanice Italy</h1>
             </div>
 
@@ -58,7 +58,7 @@
                 </div>
             </div>           
 
-            <footer class="bg-dark " style="height: 100px;">ffffffffffffffffffffffffffffffffffff</footer>
+           
 
         </div>
     </div>
