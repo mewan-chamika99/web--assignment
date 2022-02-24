08a2da723dcc964472cba411564a184e5f9c985d
Chamath Hasaranga <97539630+Chamath1999@users.noreply.github.com>
2022-02-25 02:08:55 +0530

    Update singlePlace 20.php

--- singlePlace 20.php
+++ singlePlace 20.php
@@ -22,7 +22,7 @@
             ?>
 
             <div class="col-12 a20 " style="margin-bottom: 40px;">
-                <h1 class="text-center pt-5">Cinque Italy</h1>
+                <h1 class="text-center pt-5 white">Cinque Italy</h1>
             </div>
 
             <div class="col-12 mt-2 shadow-lg section">
