c5a7a8eb1b6e6000832a6b3d123e93de371c5329
Chamath Hasaranga <97539630+Chamath1999@users.noreply.github.com>
2022-02-25 02:03:04 +0530

    Update singlePlace 11.php

--- singlePlace 11.php
+++ singlePlace 11.php
@@ -22,7 +22,7 @@
             ?>
 
             <div class="col-12 a11 " style="margin-bottom: 40px;">
-                <h1 class="text-center pt-5">Galapagos Island</h1>
+                <h1 class="text-center pt-5 white">Galapagos Island</h1>
             </div>
 
             <div class="col-12 mt-2 shadow-lg section">
