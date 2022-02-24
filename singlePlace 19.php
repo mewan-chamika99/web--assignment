4daa32bc466ea895dc0224412a9a6ecbc9840f46
Chamath Hasaranga <97539630+Chamath1999@users.noreply.github.com>
2022-02-25 01:31:56 +0530

    Update singlePlace 19.php

--- singlePlace 19.php
+++ singlePlace 19.php
@@ -21,7 +21,7 @@
 
             ?>
 
-            <div class="col-12 singleHd " style="margin-bottom: 40px;">
+            <div class="col-12 a19 " style="margin-bottom: 40px;">
                 <h1 class="text-center pt-5">Meteora Greece</h1>
             </div>
 
@@ -58,9 +58,6 @@
                     </div>
                 </div>
             </div>
-
-            <footer class="bg-dark " style="height: 100px;">ffffffffffffffffffffffffffffffffffff</footer>
-
         </div>
     </div>
 
