9280b9c014edc0ba17826ba70770848b6e985cd1
Chamath Hasaranga <97539630+Chamath1999@users.noreply.github.com>
2022-02-25 02:01:22 +0530

    Update singlePlace 8.php

--- singlePlace 8.php
+++ singlePlace 8.php
@@ -22,7 +22,7 @@
             ?>
 
             <div class="col-12 a8 " style="margin-bottom: 40px;">
-                <h1 class="text-center pt-5">Fiordland National Park</h1>
+                <h1 class="text-center pt-5 white">Fiordland National Park</h1>
             </div>
 
             <div class="col-12 mt-2 shadow-lg section">
