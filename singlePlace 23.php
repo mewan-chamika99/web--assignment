bdb0a0fefaa59820f3ac504f730c8b973e66d91f
Chamath Hasaranga <97539630+Chamath1999@users.noreply.github.com>
2022-02-25 02:10:57 +0530

    Update singlePlace 23.php

--- singlePlace 23.php
+++ singlePlace 23.php
@@ -22,7 +22,7 @@
             ?>
 
             <div class="col-12 a23 " style="margin-bottom: 40px;">
-                <h1 class="text-center pt-5">Machu Picchu</h1>
+                <h1 class="text-center pt-5 white">Machu Picchu</h1>
             </div>
 
             <div class="col-12 mt-2 shadow-lg section">
