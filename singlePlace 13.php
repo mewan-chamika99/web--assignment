80f1d89e23318aed7099feb81ee5f96c206c0261
Chamath Hasaranga <97539630+Chamath1999@users.noreply.github.com>
2022-02-25 02:04:22 +0530

    Update singlePlace 13.php

--- singlePlace 13.php
+++ singlePlace 13.php
@@ -22,7 +22,7 @@
             ?>
 
             <div class="col-12 a13 " style="margin-bottom: 40px;">
-                <h1 class="text-center pt-5">Napa Califonia</h1>
+                <h1 class="text-center pt-5 black">Napa Califonia</h1>
             </div>
 
             <div class="col-12 mt-2 shadow-lg section">
