36f237c960ada1bc363fe8d28908f4f097324535
Chamath Hasaranga <97539630+Chamath1999@users.noreply.github.com>
2022-02-25 01:58:41 +0530

    Update singlePlace 4.php

--- singlePlace 4.php
+++ singlePlace 4.php
@@ -22,7 +22,7 @@
             ?>
 
             <div class="col-12 a4 " style="margin-bottom: 40px;">
-                <h1 class="text-center pt-5">Unawatuna</h1>
+                <h1 class="text-center pt-5 black">Unawatuna</h1>
             </div>
 
             <div class="col-12 mt-2 shadow-lg section">
