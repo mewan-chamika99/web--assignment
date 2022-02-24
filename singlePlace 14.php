76b60d9b5b9f5f97ece0ffcc04fb7a6705780048
Chamath Hasaranga <97539630+Chamath1999@users.noreply.github.com>
2022-02-25 02:04:57 +0530

    Update singlePlace 14.php

--- singlePlace 14.php
+++ singlePlace 14.php
@@ -22,7 +22,7 @@
             ?>
 
             <div class="col-12 a14 " style="margin-bottom: 40px;">
-                <h1 class="text-center pt-5">Gread Ocean Road Australia</h1>
+                <h1 class="text-center pt-5 white">Gread Ocean Road Australia</h1>
             </div>
 
             <div class="col-12 mt-2 shadow-lg section">
