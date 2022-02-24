62f01acba51420ba8136230d321d718221047947
Chamath Hasaranga <97539630+Chamath1999@users.noreply.github.com>
2022-02-25 02:01:55 +0530

    Update singlePlace 9.php

--- singlePlace 9.php
+++ singlePlace 9.php
@@ -22,7 +22,7 @@
             ?>
 
             <div class="col-12 a9 " style="margin-bottom: 40px;">
-                <h1 class="text-center pt-5">Tuscany Italy</h1>
+                <h1 class="text-center pt-5 white">Tuscany Italy</h1>
             </div>
 
             <div class="col-12 mt-2 shadow-lg section">
